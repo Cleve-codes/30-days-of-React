47a77862e8b1941ff295705175fa5f9c4aa0ff66
Cleve <clevemomanyi@gmail.com>
2023-11-16 21:54:20 +0300

    hero

--- day-1-project/src/components/Nav.jsx
+++ day-1-project/src/components/Nav.jsx
@@ -1,3 +1,4 @@
+import { hamburger } from "../assets/icons";
 import { headerLogo } from "../assets/images";
 import { navLinks } from "../constants";
 
@@ -11,15 +12,18 @@ const Nav = () => {
         <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
           {navLinks.map((item) => (
             <li key={item.label}>
-                <a
+              <a
                 href={item.href}
                 className="font-motserrat leading-normal text-lg text-slate-gray"
-                >
-                    {item.label}
-                </a>
+              >
+                {item.label}
+              </a>
             </li>
           ))}
         </ul>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="hamburger" width={25} height={25} />
+        </div>
       </nav>
     </header>
   );
--- day-1-project/src/sections/Hero.jsx
+++ day-1-project/src/sections/Hero.jsx
@@ -1,5 +1,37 @@
+import { arrowRight } from "../assets/icons";
+import Button from "../components/Button";
+import { statistics } from "../constants";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p>Our Summer collection</p>
+        <h1>
+          <span>The new arrival</span>
+          <br />
+          <span>Nike</span>
+          Shoes
+        </h1>
+        <p>
+          Discover stylish Nike arrivals, quality comfort, and inovation for
+          your active life.
+        </p>
+        <Button label="Shop now" iconURL={arrowRight} />
+        <div className="flex justify-starts items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat, index) => {
+            <div key={index}>
+              <p>{stat.label}</p>
+              <p>{stat.value}</p>
+            </div>;
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
