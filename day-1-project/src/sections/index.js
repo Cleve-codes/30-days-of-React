a433bfb27a0c873e295d138bae3cf9e7e8910a0f
Cleve <clevemomanyi@gmail.com>
2023-11-16 19:11:06 +0300

    components

---
+++ day-1-project/src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import PopularProducts from "./PopularProducts";
+
+export {
+    Hero,
+    CustomerReviews,
+    Footer,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    PopularProducts,
+    SuperQuality
+}
+
