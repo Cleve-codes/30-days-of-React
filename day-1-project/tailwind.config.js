e53f46386fb4b39cbdc30f5997456014253c2bfe
Cleve <clevemomanyi@gmail.com>
2023-11-16 18:52:27 +0300

    sections

--- day-1-project/tailwind.config.js
+++ day-1-project/tailwind.config.js
@@ -19,7 +19,7 @@ export default {
         montserrat: ["Montserrat", "sans-serif"],
       },
       colors: {
-        primary: "#ECEEFF",
+        "primary": "#ECEEFF",
         "coral-red": "#FF6452",
         "slate-gray": "#6D6D6D",
         "pale-blue": "#F5F6FF",
