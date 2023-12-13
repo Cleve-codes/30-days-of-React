b531ec4a7b6001f9fdfdaf099cb3f3ea09301a62
Cleve-codes <clevemomanyi@gmail.com>
2023-12-13 21:24:41 +0300

    root

---
+++ day-2-projects/day-2-react-router-project/src/main.jsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import * as ReactDom from "react-dom/client";
+
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./index.css";
+import Root, {
+  loader as RootLoader,
+  action as RootAction,
+} from "./routes/Root";
+import ErrorPage from "./ErrorPage";
+import Contact, { loader as ContactLoader } from "./routes/Contact";
+import Edit, { action as EditAction } from "./routes/Edit";
+import Index from "./routes/Index";
+import { action as DeleteAction } from "./routes/DeleteContact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: RootLoader,
+    action: RootAction,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+      },
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: ContactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <Edit />,
+        loader: ContactLoader,
+        action: EditAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: DeleteAction,
+        errorElement: <div>Opps! There was an error.</div>,
+      },
+      {
+        path: "*",
+        element: <div>Not Found</div>,
+      },
+    ],
+  },
+]);
+
+ReactDom.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ day-2-projects/day-2-react-router-project/src/routes/Index.jsx
@@ -0,0 +1,15 @@
+ const Index = () => {
+  return (
+    <p className="zero-state">
+      This is a demo for React Router.
+      <br />
+      Check Out
+      <a href="https://reactrouter.com">
+        the docs at reactrouter.com;
+      </a>
+      .
+    </p>
+  );
+}
+
+ export default Index;
