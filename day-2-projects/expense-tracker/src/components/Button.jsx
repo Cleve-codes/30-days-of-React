383c3b5bed2417614c5c2325e96626bfa8e02cd9
Cleve-codes <clevemomanyi@gmail.com>
2023-12-19 18:22:17 +0300

    navigate

--- day-2-projects/expense-tracker/src/components/Button.jsx
+++ day-2-projects/expense-tracker/src/components/Button.jsx
@@ -1,10 +1,14 @@
-const Button = ({ text }) => {
+import { Link } from "react-router-dom";
+
+const Button = ({ text, to, onClick }) => {
   return (
     <button className="cursor-pointer group relative flex gap-1.5 
     px-8 py-4 bg-black bg-opacity-80 text-[#f1f1f1] 
     rounded-2xl hover:bg-opacity-70 
-    transition font-semibold shadow-md" >
-      {text}
+    transition font-semibold shadow-md" onClick={onClick} >
+      <Link to={to}>
+        {text}
+      </Link>
     </button>
   )
 }
--- day-2-projects/expense-tracker/src/pages/ErrorPage.jsx
+++ day-2-projects/expense-tracker/src/pages/ErrorPage.jsx
@@ -1,13 +1,20 @@
-import { useRouteError } from 'react-router-dom';
 import errorImg from '../assets/error-400.jpg'
+
+import { useNavigate, useRouteError } from "react-router-dom";
+import Button from '../components/Button';
 
 const ErrorPage = () => {
   const error = useRouteError();
-  console.error(error);
+  const navigate = useNavigate();
 
   return (
-    <div className='max-w-5xl max-h-[90vh] flex justify-center items-center ml-[20%]'>
-      <img src={errorImg} className='w-1/2 h-1/2 mt-[20%]' alt="error" />
+    <div className='max-w-5xl max-h-[90vh] flex flex-col justify-center items-center ml-[25%]'>
+      <p>{error}</p>
+      <img className='mt-[20%] w-[400px] h-[400px]' src={errorImg} alt='error Image' />
+      <div className='flex gap-[3em] mt-[2em]'>
+        <Button text="Go Back Home 🛖" to='/' />
+        <Button text="Go Back👈🏾 " onClick={() => navigate(-1)} />
+      </div>
     </div>
   )
 }
